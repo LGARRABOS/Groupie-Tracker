5930838a9e243bf377eec94442cfc3b65ad86669
AFERREIRA33 <alex.ferreira@ynov.com>
2022-01-21 15:55:29 +0100

    debut of json control

---
+++ function/MainPage.go
@@ -0,0 +1,17 @@
+package groupie
+
+import (
+	"net/http"
+	"html/template"
+)
+
+func MainHandler(http ReponseWriter, r *http.Request ) {
+	tmpl := template.Must(template.ParseFiles("../static/index.html"))
+	
+	APIRequest()
+	data := ArtisStruct {
+		Tab: ArtistTab
+	}
+	
+	tmpl.Execute(w, data)
+}
---
+++ server/server.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+	g  "groupie/function"
+)
+func main() {	
+	fs := http.FileServer(http.Dir("../static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.ListenAndServe("localhost:8080", nil)
+	g.MainHandler()
+}
