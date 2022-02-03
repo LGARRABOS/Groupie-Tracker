62a1e0aa0f377bd59d6cb4fe4c2be96a219501d3
LGARRABOS <luc.garrabos@ynov.com>
2022-02-03 10:41:47 +0100

    card index page

--- function/MainPage.go
+++ function/MainPage.go
@@ -1,16 +1,14 @@
 package groupie
 
 import (
-	"net/http"
 	"html/template"
+	"net/http"
 )
-
-
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("../static/index.html"))
-	
+
 	data := APIRequestArtist()
-	
+
 	tmpl.Execute(w, data)
 }
