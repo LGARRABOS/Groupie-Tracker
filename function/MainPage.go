b518b098d473550a9cabfa8f49cf3dfc2ceaa6ed
AFERREIRA33 <alex.ferreira@ynov.com>
2022-02-03 10:41:20 +0100

    button location post method

--- function/MainPage.go
+++ function/MainPage.go
@@ -3,6 +3,7 @@ package groupie
 import (
 	"net/http"
 	"html/template"
+	"fmt"
 )
 
 
@@ -11,6 +12,18 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("../static/index.html"))
 	
 	data := APIRequestArtist()
+
+	switch r.Method {
+	case "GET":
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		fmt.Printf("POST METHOD = %v\n", r.PostForm)
+
+	}
+
 	
 	tmpl.Execute(w, data)
 }
