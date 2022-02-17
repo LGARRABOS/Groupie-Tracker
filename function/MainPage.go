275868cc1868797a07e25c9eb8578e2009acc2ce
LGARRABOS <luc.garrabos@ynov.com>
2022-02-17 09:31:11 +0100

    cars css

--- function/MainPage.go
+++ function/MainPage.go
@@ -7,7 +7,7 @@ import (
 )
 
 func MainHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("../static/index.html"))
+	tmpl := template.Must(template.ParseFiles("../static/card.html"))
 	data := APIRequestArtist()
 
 	switch r.Method {
